94b04c0e519599514a9a6ef163cd8be8c2991504
boredompwndu <thombuis@gmail.com>
2018-01-13 00:07:36 -0500

    initial Ranger orders and action tree
    Works similarly to last year's bots. Each bot has a given task and a location for that task. It attempts to move to that location and perform its task on the way.
    
    the attack mode is standard attack move to a lcoation.
    
    protect mode will go to a location, attempt to find a unit. if it finds a unit, it orbits the unit, attacking enemies as it goes.
    
    The move mode goes to the destination without attacking.
    
    todo:snipe, idle, and wait modes, actually populating the modes

--- src/RangerBot.java
+++ src/RangerBot.java
@@ -1,10 +1,49 @@
 import bc.GameController;
 
 public class RangerBot extends BasicBot {
+	
+	GameController gc;
+	public enum Command{Protect,Attack,Move,Wait};
+	public Command currentTask;
+	public int targetID=-1;
+	public int targetX =-1;
+	public int targetY =-1;
+	
 
 	public RangerBot(GameController gc, int unitID) {
 		super(gc, unitID);
 		// TODO Auto-generated constructor stub
+		this.gc = gc;
 	}
+	
+	public void activate() {
+		switch(currentTask){
+		case Attack:
+			attack();
+			break;
+		case Move:
+			move();
+			break;
+		case Protect:
+			protect();
+			break;
+		}
+	}
+	
+	public void attack() {
+		
+	}
+	
+	public void protect() {
+		
+	}
+	
+	public void move() {
+		
+	}
+	
+	public void 
+	
+	
 
 }
